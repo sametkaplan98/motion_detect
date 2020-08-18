37a89e774f04d2155303d284c537f3f23ea4001d
Samet <47507568+sametkaplan98@users.noreply.github.com>
2020-08-18 23:05:35 +0300

    Update FLANN_video_test.py

--- FLANN_video_test.py
+++ FLANN_video_test.py
@@ -1,28 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Aug 14 01:07:38 2020
-
-@author: Samet
-"""
-
-# -*- coding: utf-8 -*-
-"""
 Created on Fri Aug 14 00:11:05 2020
 
 @author: Samet
 """
 import cv2
-from matplotlib import pyplot as plt
 import numpy as np
 import imutils
 import time
 
 MIN_MATCH_COUNT=25
-
-
-
 video=cv2.VideoCapture(1)
-
 
 while True:
     
@@ -31,14 +19,13 @@ while True:
  orjinal=imutils.resize(orjinal,360,360)
  time.sleep(0.1)   
           
- a,frame2=video.read()
+ _,frame2=video.read()
 
- 
  img1=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
  img1=cv2.GaussianBlur(img1,(15,15),0)
  
  img2=cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
- img2=cv2.GaussianBlur(img2,(15,15),0)
+ img2=cv2.GaussianBlur(img2,(15,15),0)  
 
  sift=cv2.xfeatures2d.SIFT_create()
  
@@ -55,11 +42,8 @@ while True:
  toplam_dst_y=0
  toplam_src_y=0
  
- poz1_x=0
- pox2_x=0
  
  if des2 is not None : # Ortak feature bulunmadigi durumda hata vermemesi icin
-  
 
   FLANN_INDEX_KDTREE=0
   index_params=dict(algorithm=FLANN_INDEX_KDTREE)          
@@ -89,9 +73,6 @@ while True:
    src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)    
    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
    
-   
-   
-   
    for j in src_pts:                        ## Birinci frame 
        #print(j[0,0])
        toplam_src_x=toplam_src_x+j[0,0]
@@ -99,12 +80,9 @@ while True:
        sayac_src+=1  
    x_src_ortalama=toplam_src_x/sayac_src
    y_src_ortalama=toplam_src_y/sayac_src
-   #print('1.Frame ortalama Y: ',y_src_ortalama)
-   
+   #print('1.Frame ortalama Y: ',y_src_ortalama)  
    cv2.circle(img1,(int(x_src_ortalama),int(y_src_ortalama)),6,(255,0,255),2)
-   
-   
-   
+    
    for i in dst_pts:                          ## İkinci frame 
        #print(i[0,0])
        toplam_dst_x=toplam_dst_x+i[0,0]
@@ -114,7 +92,6 @@ while True:
    y_dst_ortalama=toplam_dst_y/sayac_dst
    #print('2.Frame ortalama Y: ',y_dst_ortalama)
    cv2.circle(img2,(int(x_dst_ortalama),int(y_dst_ortalama)),6,(255,0,255),2)
-
    
    #------------------------------------------------------------------------------------------
    """
@@ -133,8 +110,7 @@ while True:
        print('YUKARI')
    elif (y_src_ortalama-y_dst_ortalama)> 0:
        print('ASAGI')
-  
-   
+     
    """ 
     #---------------------------------------------------------------------------------
        
@@ -168,9 +144,6 @@ while True:
    elif abs(y_src_ortalama-y_dst_ortalama) <4 and abs(y_src_ortalama-y_dst_ortalama) >0 and(x_src_ortalama-x_dst_ortalama) < 0:
        
        print('X Asagi + Y:0')
-       
-   
-
    
    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0) 
    
@@ -180,11 +153,9 @@ while True:
    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)    
 
    dst = cv2.perspectiveTransform(pts,M)
-   
-  
+     
    img2=cv2.polylines(img2,[np.int32(dst)],True,255,3,cv2.LINE_AA)
- 
-  
+   
   draw_params=dict(matchColor=(0,255,0),singlePointColor=None,
                  matchesMask=matchesMask,
                  flags=2)
@@ -198,8 +169,6 @@ while True:
    cv2.imshow('Sonuc ( Birinci Frame ve Ikinci Frame',img3)
   else:
       cv2.imshow('Sonuc ( Birinci Frame ve Ikinci Frame',img2)
-
-  
   
   if cv2.waitKey(1) &  0xFF == ord('q'):
      break
@@ -209,7 +178,3 @@ while True:
 
 video.release()
 cv2.destroyAllWindows()
-
-
-
-
